1ddde9a9e3a688884345306ce7c569853c1e1790
Denis Giroto <girotodenis@gmail.com>
2022-11-12 23:40:37 -0300

    passo1

---
+++ ifood/cadastro/src/test/java/br/dsg/ifood/cadastro/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package br.dsg.ifood.cadastro;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
