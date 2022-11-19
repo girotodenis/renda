d8470736f3c795ff4f88b1230c2bd98650293282
Denis Giroto <girotodenis@gmail.com>
2022-11-19 15:20:23 -0300

    teste

--- ifood/cadastro/src/test/java/br/dsg/ifood/cadastro/RestauranteResourceTest.java
+++ ifood/cadastro/src/test/java/br/dsg/ifood/cadastro/RestauranteResourceTest.java
@@ -1,21 +1,33 @@
 package br.dsg.ifood.cadastro;
 
-import io.quarkus.test.junit.QuarkusTest;
+import static io.restassured.RestAssured.given;
+
+import com.github.database.rider.cdi.api.DBRider;
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.configuration.Orthography;
+import com.github.database.rider.core.api.dataset.DataSet;
+
+import org.approvaltests.JsonApprovals;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import io.quarkus.test.junit.QuarkusTest;
 
+@DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
 @QuarkusTest
 public class RestauranteResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
+    @DataSet("restaurante-cenario-1.yml")
+    public void test_restaurantes_endpoint() {
+        var string = given()
           .when().get("/restaurantes")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .extract().asString();
+        
+        JsonApprovals.verifyJson(string);
+        
     }
 
 }
