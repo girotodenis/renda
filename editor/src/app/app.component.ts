6362d8274edfbc1f0c861543e83267f7570ec5d2
Denis Giroto <girotodenis@gmail.com>
2025-03-22 11:15:55 -0300

    tynimce

--- editor/src/app/app.component.ts
+++ editor/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { EditorComponent } from '@tinymce/tinymce-angular';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [EditorComponent],
+  template: `
+  <h1>TinyMCE 7 Angular Demo</h1>
+  <editor
+    [init]="init"
+  />
+  `
 })
 export class AppComponent {
-  title = 'editor';
+  init: EditorComponent['init'] = {
+    base_url: '/tinymce', // Root for resources
+    skin: 'tinymce-5',
+    license_key: 'gpl',
+    suffix: '.min'  ,
+    min_height: 500,
+    language: 'pt_BR',
+    contextmenu: 'bold italic underline',
+    autoresize_min_height: 650,
+    pagebreak_split_block: true,
+    menubar: true,
+    //  toc lineheight print hr paste textcolor colorpicker textpattern imagetools
+    plugins: 'variaveisMesclagem fullscreen codesample insertdatetime advlist media lists autoresize save searchreplace nonbreaking link image charmap preview anchor pagebreak visualblocks visualchars code table directionality help',
+    external_plugins: {
+      variaveisMesclagem: 'plugins/variaveisMesclagem/plugin.js'
+    },
+    toolbar: `showVariable | undo redo | fontselect | fontsizeselect | styleselect | bold italic underline forecolor backcolor` +
+    ` | link image | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent` +
+    ` | removeformat | lineheightselect`,
+  };
 }
---
+++ editor/src/app/assets/tinymce/plugins/variaveisMesclagem/plugin.js
@@ -0,0 +1,103 @@
+tinymce.PluginManager.add('variaveisMesclagem', function (editor) {
+  
+  var showVariable = false; // Valor inicial do botão
+
+  let variables = {};
+
+  editor.on('loadVariables', function (data) {
+      console.warn('editor.loadVariables',data)
+      console.warn('editor.loadVariables',data.variablesEvent)
+      variables = data.variablesEvent;
+      if (showVariable) {
+        resolverSpansDoTemplate();
+      }
+  });
+
+  editor.on('loadContent', function (data) {
+      console.warn('editor.loadContent')
+      if (showVariable) {
+        resolverSpansDoTemplate();
+      }
+  });
+  
+  editor.ui.registry.addButton('showVariable', {
+    text: '< - >',
+    //text: '<--hide-->',
+    //icon: 'eye',
+    onAction: function(botao) {
+          showVariable = !showVariable; // Alterna o valor do botão
+
+          // botao.setText(showVariable ? '<show>' : '--hide--'); // Alterna o texto do botão
+          botao.setText(showVariable ? '<o>' : '< - >'); // Alterna o texto do botão
+          // botao.icon(showVariable ? 'eye-slash' : 'eye'); // Alterna o ícone do botão
+
+          resolverSpansDoTemplate();
+        }
+    });
+
+
+    let resolverSpansDoTemplate = () => {
+      // Pega todos os elementos com a classe 'variaveisMesclagem'
+      let elements = editor.dom.select('.variaveisMesclagem');
+      elements.forEach(function(element) {
+        // Pega o id do variável (que é o atributo data-var do elemento)
+          var spanVar = Array.from(element.attributes).find(function(elemento) {
+            return elemento.name == 'data-var';
+          });
+          if(spanVar){
+            let variableName = spanVar.nodeValue;
+            // Se o botão estiver em 'show', mostrar o valor da variável
+            if (showVariable) {
+              
+                element.innerHTML = '';
+                element.innerText = '';
+                var meuSpan = editor.dom.create('span');
+                meuSpan.textContent = variables[variableName];
+                element.appendChild(meuSpan);
+
+                editor.dom.addClass(element, 'valorMesclado');
+              }
+            // Se o botão estiver em 'hide', mostrar o nome da variável
+            else {
+              element.innerHTML = '';
+              element.innerText = '';
+              var meuSpan = editor.dom.create('span');
+              meuSpan.textContent = '@'+variableName;
+              meuSpan.style.color = 'navy';
+              meuSpan.style.fontStyle = 'italic';
+              element.appendChild(meuSpan);
+              editor.dom.removeClass(element, 'valorMesclado')
+            }
+          }
+      });
+    }
+
+    editor.ui.registry.addAutocompleter('variables', {
+    ch: '@',
+    minChars: 0,
+    columns: 1,
+    fetch: function (pattern) {
+
+      let matchedVariables = Object.keys(variables).filter(item => item.includes(pattern));
+      return new Promise(function (resolve) {
+        resolve(matchedVariables.map(function (item) {
+          return {
+            value: `<span data-var="${item}" class="variaveisMesclagem"><span style="color: navy; font-style: italic;">@${item}</span></span> `,
+            text: item + ' = ' + variables[item],
+            icon: '@'
+          };
+        }));
+      });
+    },
+    onAction: function (autocompleteApi, rng, value) {
+      editor.selection.setRng(rng);
+      editor.insertContent(value);
+      if (showVariable) {
+        resolverSpansDoTemplate();
+      }
+      autocompleteApi.hide();
+    }
+  });
+
+
+  });
